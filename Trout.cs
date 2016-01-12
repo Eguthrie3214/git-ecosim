884065db770d6deafb313aa3ac310ce3a2a4c9fa
Eguthrie3214 <eguthrie3214@gmail.com>
2016-01-12 13:34:11 -0800

    2 more fish
    forgot 2 fish

---
+++ Trout.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets;
+
+public class Trout : MonoBehaviour
+{
+
+    fish trout = new fish();
+    public int troutAge;
+    public int hunger;
+    Transform flip;
+    Rigidbody2D rig;
+    public Vector3 realSize;
+    public float adjustedsize;
+    public float growthperseason;
+
+
+
+    
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collider2D mouth = GetComponent<BoxCollider2D>();
+        trout.eat(trout.hunger, mouth, collision.gameObject.GetComponent<Collider2D>());
+    }
+
+    
+    
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        flip = GetComponent<Transform>();
+        realSize = GetComponent<SpriteRenderer>().bounds.size;
+
+
+        trout.name = "trout" + GetInstanceID().ToString();
+        name = trout.name;
+
+        trout.hunger = 0;
+        trout.maxLifespan = 11;
+
+        trout.scaleMaxSize = Random.Range(.60f,.90f);
+        trout.scaleStartSize = Random.Range(.15f, .30f);
+        flip.localScale = new Vector3(trout.scaleStartSize, trout.scaleStartSize);
+
+
+        trout.spriteSizeX = realSize.x;
+        trout.spriteSizeY = realSize.x;
+        
+
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+       
+        adjustedsize = trout.adjustedSpriteSize;
+        troutAge = trout.interaction();
+        hunger = trout.hunger;
+        growthperseason = trout.grow(flip);
+        trout.swim(rig, flip);
+
+    }
+}
---
+++ smallMinnow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets; 
+
+public class smallMinnow : MonoBehaviour {
+
+    fish minnow = new fish();
+    public Vector3 realsize;
+    Transform flip;
+    Rigidbody2D rig;
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collider2D mouth = GetComponent<BoxCollider2D>();
+        minnow.eat(minnow.hunger, mouth, collision.gameObject.GetComponent<Collider2D>());
+    }
+    // Use this for initialization
+    void Start () {
+        minnow.name = "minnow" + GetInstanceID().ToString();
+        rig = GetComponent<Rigidbody2D>();
+        flip = GetComponent<Transform>();
+        minnow.hunger = 0;
+        realsize = GetComponent<SpriteRenderer>().bounds.size;
+        minnow.spriteSizeX = realsize.x;
+        minnow.spriteSizeY = realsize.y;
+        name = minnow.name;
+        minnow.hunger = 0;
+        minnow.maxLifespan = 6;
+        minnow.scaleMaxSize = Random.Range(.5f, .6f);
+        minnow.scaleStartSize = Random.Range(.05f, .15f);
+        flip.localScale = new Vector3(minnow.scaleStartSize, minnow.scaleStartSize);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        minnow.interaction();
+        minnow.grow(flip);
+        minnow.swim(rig,flip);
+	}
+}
